4af0d8a735028b3009ff72c9696529a743347526
mbaron93 <mbaron@herronhighschool.org>
2025-03-26 16:34:32 +0000

    Added in timestamp with DateTime function when a source is created. Added accessor to value of each

--- SecondBrain/SecondBrain/Source.cs
+++ SecondBrain/SecondBrain/Source.cs
@@ -1,17 +1,29 @@
 namespace SecondBrain; 
 
+using System; 
+
 //The source class, which represents each information source
 public class Source{
    private string title; 
    private string author; 
    private string notes; 
    private double amount; 
+   public DateTime started; 
+
+    public Source(string t, string a, string n, double amount, DateTime s){
+    title = t; 
+    author = a; 
+    notes = n; 
+    this.amount = amount; 
+    started = s;
+   }
 
    public Source(string t, string a, string n, double amount){
     title = t; 
     author = a; 
     notes = n; 
     this.amount = amount; 
+    started = DateTime.Now; 
    }
 
    public Source(string t, string a, string n){
@@ -19,6 +31,7 @@ public class Source{
     author = a; 
     notes = n; 
     this.amount = 100.0; 
+    started = DateTime.Now; 
    }
    
    public string getTitle(){
@@ -34,6 +47,9 @@ public class Source{
     return amount; 
    }
    public string toString(){
-    return "Title: "+title+"\nAuthor: "+author+"\nNotes: "+notes+"\nAmount: "+getAmount(); 
+    return "Title: "+title+"\nAuthor: "+author+"\nNotes: "+notes+"\nAmount: "+getAmount() + "\nStarted on "+started.Month + "/"+started.Day; 
+   }
+   public DateTime getStart(){
+      return started; 
    }
 }
