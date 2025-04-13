d98ee2a450fe9af85e08e573a7c8bab2d9046b92
mbaron93 <mbaron@herronhighschool.org>
2025-04-13 01:48:54 +0000

    File Reorganization - Updating AddSource to fix bug

--- SecondBrain/SecondBrain/Program.cs
+++ SecondBrain/SecondBrain/Program.cs
@@ -9,8 +9,8 @@ class Program
 
     static void Main(string[] args)
     {
-        DataManager dm = new DataManager("Sources.csv");
-        GoalManager gm = new GoalManager("Goals.csv");
+        DataManager dm = new DataManager("Data/Sources.csv");
+        GoalManager gm = new GoalManager("Data/Goals.csv");
         string[] options = new String[] {
             "Add a source", "Edit a source", "Set or edit a Goal", "View Learning Data", "Exit"};
         UI u = new UI(options, dm, gm); 
